b5602f53a0bf6e5f831a1f6e954552df5b2ea498
Kenny <seo.kenny94@gmail.com>
2019-03-31 11:34:39 -0400

    Base blueprint with rules

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**ZOMBIE DICE RULES 
+13 Custom Dice
+Win Condition: First to 13 brains wins.
+You take 3 random dice from the box and roll them.
+
+A brain is worth 1 point at the end of the round.
+Footsteps allow you to reroll that dice.
+If you collect 3 shotgun blasts, you lose all the points for that round.
+
+After rolling 3 dice, you can choose to hold and save the points, or
+keep pushing and roll 3 more dice.
+**/
